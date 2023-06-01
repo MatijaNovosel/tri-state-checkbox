888bfaf7dae76b5a303484807f0d77bef092cfae
Matija Novosel <mnovosel5@gmail.com>
2023-06-02 00:00:40 +0200

    Tweak

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <vue-tri-state-checkbox v-model="val" label="Vite + Vue" />
+  <tri-state-checkbox v-model="val" label="Vite + Vue" />
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import TriStateCheckbox from "./components/TriStateCheckbox.vue";
 
 const val = ref(false);
 </script>
